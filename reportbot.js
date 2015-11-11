9de7ab8fe4fdf07eff6b7b3ebcf532d78f70dec6
ctong1234 <calvinytong@gmail.com>
2015-11-10 19:38:33 -0500

    asdf

---
+++ reportbot.js
@@ -0,0 +1,62 @@
+var fs = require('fs');
+var Parse = require('parse/node').Parse;
+var clientkey = fs.readFileSync('./.client_key').toString().replace(/(\r\n|\n|\r)/gm,"");
+var javascriptkey = fs.readFileSync('./.javascript_key').toString().replace(/(\r\n|\n|\r)/gm,"");
+
+Parse.initialize(clientkey, javascriptkey);
+
+module.exports = function (req, res, next) {
+  console.log(clientkey);
+  var text = req.body.text;
+  var tok = text.split(" ");
+  console.log(tok);
+  //if wrong syntax put back
+  if(tok.length != 2){
+    var botPayload = {
+      text : "that is not how you report people"
+    };
+    return res.status(200).json(botPayload);
+  }
+  var People = Parse.Object.extend("People");
+  var query = new Parse.Query(People);
+  query.equalTo("name", tok[1]);
+  query.find({
+  success: function(people) {
+    if(people.length == 0){
+      var object = new People();
+      object.set("name", tok[1]);
+      object.set("reports", 1);
+      object.save(null, {
+        success: function(object) {
+          var botPayload = {
+            text : tok[1] + " has been reported 1 time"
+          }
+          return res.status(200).json(botPayload);
+        },
+        error: function(gameScore, error) {
+          alert('Failed to create new object, with error code: ' + error.message);
+        }
+    });
+    }
+    else{
+      var reportee = people[0];
+      var reports = reportee.get("reports") + 1;
+      reportee.set("reports", reports);
+      reportee.save(null, {
+        success: function(object){
+        },
+        error: function(object){
+          alert("failed to create object");
+        }
+      });
+      var botPayload = {
+        text : tok[1] + " has been reported " + reports + " times"
+      };
+      return res.status(200).json(botPayload);
+    }
+  },
+  error: function(error) {
+    alert("Error: " + error.code + " " + error.message);
+  }
+  });
+}
